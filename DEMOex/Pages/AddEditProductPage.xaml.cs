95adc87336037496427e3c2b590541cf11108bd2
bulat <valiaxmetovb134@gmail.com>
2023-05-04 16:06:44 +0300

    finished with add edit, added validation

--- DEMOex/Pages/AddEditProductPage.xaml.cs
+++ DEMOex/Pages/AddEditProductPage.xaml.cs
@@ -2,7 +2,9 @@
 using DEMOex.Models.Entities;
 using DEMOex.Navigation;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -22,8 +24,18 @@ namespace DEMOex.Pages
             InitializeComponent();
             page1 = page;
 
-            if (product != null)
-                _product = product;
+            _product = product;
+
+            if (_product.ProductId != 0)
+            {
+                tbProductArticleNumber.IsEnabled = false;                
+            }
+            else
+            {
+                tbProductArticleNumber.IsEnabled = true;
+            }
+            
+                
 
             DataContext = _product;
 
@@ -43,15 +55,47 @@ namespace DEMOex.Pages
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            if (_product.ProductId == 0)
+            StringBuilder errors = new StringBuilder();
+            var stackPanels = addEditGrid.Children.OfType<StackPanel>();
+            List<TextBox> textBoxes = new List<TextBox>();
+            List<ComboBox> comboBoxes = new List<ComboBox>();
+
+            foreach (StackPanel sp in stackPanels)
             {
-                _context.Products.Add(_product);
+                textBoxes.AddRange(sp.Children.OfType<TextBox>());
+                comboBoxes.AddRange(sp.Children.OfType<ComboBox>());
+            }
+
+            foreach (var item in textBoxes)
+            {
+                if (string.IsNullOrWhiteSpace(item.Text))
+                {
+                    errors.AppendLine($"Поле: {item.Name} не может быть пустым");
+                }
+            }
+            foreach (var item in comboBoxes)
+            {
+                if (item.SelectedItem == null)
+                {
+                    errors.AppendLine($"Поле: {item.Name} не может быть пустым");
+                }
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
             }
 
             _product.ProductCategory = comboCategory.SelectedItem as ProductCategory;
             _product.ProductManufacturer = comboManufacturer.SelectedItem as ProductManufacturer;
             _product.ProductSupplier = comboSupplier.SelectedItem as ProductSupplier;
             _product.UnitType = comboUnitType.SelectedItem as UnitType;
+
+            if (_product.ProductId == 0)
+            {
+                _context.Products.Add(_product);
+            }            
 
             try
             {
@@ -60,12 +104,19 @@ namespace DEMOex.Pages
                 ((AdminPage)page1).lvProducts.ItemsSource = _context.Products.ToList();
                 ((AdminPage)page1).ComboBoxFilterProductByPrice.SelectedIndex = 0;
                 ((AdminPage)page1).ComboBoxFilterProductDiscountAmount.SelectedIndex = 0;
-                MainNavigationManager.MainFrame.GoBack();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
 }
+
+        private void btnReturnToProductPage_Click(object sender, RoutedEventArgs e)
+        {
+            if ((MessageBox.Show("Не сохраненные данные будут потеряны. Продолжить?", "Возврат на страницу", MessageBoxButton.YesNo, MessageBoxImage.Question)) == MessageBoxResult.Yes)
+            {
+                MainNavigationManager.MainFrame.GoBack();
+            }               
+        }
     }
 }
